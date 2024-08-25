b1447e3588acdb402368f7ed578eb29ff3a39efc
SaulFernandezGarcia <saulfernandez2003@gmail.com>
2024-08-25 17:28:25 +0200

    WindowManager obtained a locator for the required window. Nonetheless, doesn't pass test_obtain_tabs_when_repeated_links_first_middle(self, mock_window, mock_copy_paste_link, mock_next_tab, mock_previous_tab). Need to include: - emergency button! Press "esc" key to stop the code execution. - also, just in case, check before executing shortcuts that the current window is the desired one (in case the user does any kind of interaction with the keyboard that switches tabs).

--- src/utils/window_manager.py
+++ src/utils/window_manager.py
@@ -2,27 +2,66 @@ import pygetwindow as gw
 import pyautogui as pg
 import pyperclip
 
+# from pywinauto import Application
+
 class WindowManager:
+
     @staticmethod
     def obtain_tabs(w: gw.Window):
-        w.maximize()
+
+        WindowManager.locate_window(w)
 
         url_list = []
-        url_list.append(WindowManager.copy_paste_link())
+
+        first_url = WindowManager.copy_paste_link()
+        url_list.append(first_url)
         WindowManager.next_tab()
         while True:
             new_url = WindowManager.copy_paste_link()
-            WindowManager.next_tab()
-            if (url_list[0] == new_url):
+            if (new_url == first_url):
                 break
-            else:
-                url_list.append(new_url)
+
+            url_list.append(new_url)
+            WindowManager.next_tab()
+
         print(url_list)
+
         return url_list
+
+    @staticmethod
+    def locate_window(w):
+        i = 1
+        while not WindowManager.is_desired_window_on_foreground(w):
+            WindowManager.previous_window()
+
+    @staticmethod
+    def get_foreground_window():
+        active_window = gw.getActiveWindow()
+        return active_window
+
+    @staticmethod
+    def is_desired_window_on_foreground(desired_window: gw.Window):
+        current_foreground = WindowManager.get_foreground_window()
+        return current_foreground._hWnd == desired_window._hWnd
+
+    #___________________________________________________________
+
+    @staticmethod
+    def previous_window():
+        pg.hotkey("alt", "shift", "tab")
+
+    @staticmethod
+    def next_window():
+        pg.hotkey("alt", "tab")
+        #GOOD LORD FIX THIS, MAKE IT A REQUIREMENT TO HAVE THE TAB AS NEXT IMMEDIATE TAB
 
     @staticmethod
     def next_tab():
         pg.hotkey("ctrl", "tab")
+
+    @staticmethod
+    def previous_tab():
+        pg.hotkey("ctrl", "shift", "tab")
 
     @staticmethod
     def copy_paste_link():
--- test/utils/test_window_manager.py
+++ test/utils/test_window_manager.py
@@ -10,12 +10,16 @@ class TestWindowManager(TestCase):
     '''
     Important: patch order is important! The uppermost patch is the rightmost parameter!
     '''
+    @patch("src.utils.window_manager.WindowManager.previous_tab")
+    @patch("src.utils.window_manager.WindowManager.next_tab")
     @patch("src.utils.window_manager.WindowManager.copy_paste_link")
     @patch("pygetwindow.Window")
-    def test_obtain_tabs_when_no_repeated_links(self, mock_window, mock_copy_paste_link):
+    def test_obtain_tabs_when_no_repeated_links(self, mock_window, mock_copy_paste_link, mock_next_tab, mock_previous_tab):
 
         mock_window.maximize = MagicMock()
         mock_window.minimize = MagicMock()
+        mock_next_tab = MagicMock()
+        mock_previous_tab = MagicMock()
 
         captured_tabs = [
             "https://saulfernandezgarcia.github.io",
@@ -34,26 +38,61 @@ class TestWindowManager(TestCase):
 
         self.assertListEqual(result, expected_tabs)
 
-    # def test_obtain_tabs_when_repeated_links_first_last(self):
-    #
-    #     testCapturedTabs = ["https://saulfernandezgarcia.github.io",
-    #                         "https://www.google.com",
-    #                         "https://www.youtube.com",
-    #                         "https://saulfernandezgarcia.github.io"]
-    #
-    #
-    #     self.assertListEqual()
-    #
-    # def test_obtain_tabs_when_repeated_links_first_middle(self):
-    #
-    #     testCapturedTabs = ["https://saulfernandezgarcia.github.io",
-    #                         "https://www.google.com",
-    #                         "https://www.youtube.com",
-    #                         "https://saulfernandezgarcia.github.io",
-    #                         "https://www.google.com"]
-    #
-    #
-    #     self.assertListEqual()
+    @patch("src.utils.window_manager.WindowManager.previous_tab")
+    @patch("src.utils.window_manager.WindowManager.next_tab")
+    @patch("src.utils.window_manager.WindowManager.copy_paste_link")
+    @patch("pygetwindow.Window")
+    def test_obtain_tabs_when_repeated_links_first_last(self, mock_window, mock_copy_paste_link, mock_next_tab, mock_previous_tab):
+        mock_window.maximize = MagicMock()
+        mock_window.minimize = MagicMock()
+        mock_next_tab = MagicMock()
+        mock_previous_tab = MagicMock()
+
+        captured_tabs = [
+            "https://saulfernandezgarcia.github.io",
+            "https://www.google.com",
+            "https://www.youtube.com",
+            "https://saulfernandezgarcia.github.io",
+            "https://saulfernandezgarcia.github.io"
+        ]
+
+        expected_tabs = ["https://saulfernandezgarcia.github.io",
+                            "https://www.google.com",
+                            "https://www.youtube.com"]
+
+        mock_copy_paste_link.side_effect = captured_tabs
+
+        result = WindowManager.obtain_tabs(mock_window)
+
+        self.assertListEqual(result, expected_tabs)
+
+    @patch("src.utils.window_manager.WindowManager.previous_tab")
+    @patch("src.utils.window_manager.WindowManager.next_tab")
+    @patch("src.utils.window_manager.WindowManager.copy_paste_link")
+    @patch("pygetwindow.Window")
+    def test_obtain_tabs_when_repeated_links_first_middle(self, mock_window, mock_copy_paste_link, mock_next_tab, mock_previous_tab):
+
+        mock_window.maximize = MagicMock()
+        mock_window.minimize = MagicMock()
+        mock_next_tab = MagicMock()
+        mock_previous_tab = MagicMock()
+
+        captured_tabs = ["https://saulfernandezgarcia.github.io",
+                            "https://www.google.com",
+                            "https://www.youtube.com",
+                            "https://saulfernandezgarcia.github.io",
+                            "https://www.google.com/maps"]
+
+        expected_tabs = ["https://saulfernandezgarcia.github.io",
+                         "https://www.google.com",
+                         "https://www.youtube.com",
+                         "https://www.google.com/maps"]
+
+        mock_copy_paste_link.side_effect = captured_tabs
+
+        result = WindowManager.obtain_tabs(mock_window)
+
+        self.assertListEqual(result, expected_tabs)
 
     def test_next_tab(self):
         # No idea how to test this.
